5327a5bca81d8ab62a0977dc6ec7817c6ef26e92
m-ick <11mickm@gmail.com>
2024-08-09 16:20:37 +0000

    finished

--- main.cpp
+++ main.cpp
@@ -1,50 +1,37 @@
 #include "mbed.h"
-#include <cstdio>
 
 /*
     This program follows the given flow chart
 */
 
-InterruptIn blue_btn(PC_13);
-DigitalOut led_A(LED1);
-DigitalOut led_B(LED2);
+InterruptIn blue_btn(PC_13);    // initialise Digital in (InterruptIn)
+DigitalOut led_A(LED1);         // initialise Digital Out 1
+DigitalOut led_B(LED2);         // initialise Digital Out 2
 
-volatile int flag;
+volatile int flag;              // initialise flag
 
-void btnFall(){
-    if (flag == 0) {
-        flag = 1;
-        led_A = !led_A;
-        led_B= !led_B;
-    }
-    
+void btnFall(){                 // button pressed
+    if (flag == 0) {            // checks if flag is 0
+        flag = 1;               // if flag is 0, set flag = 1
+        led_A = !led_A;         // toggle led_A
+        led_B = !led_B;          // toggle led_B
+    } 
 }
 
 void btnRise(){
-    flag = 2;
+    flag = 0;                   // button released, set flag = 0
 }
-int main()
-{ 
-    flag = 0;
-    led_A = 1;
-    led_B = 0;
 
-    // blue_btn.fall(&btnFall);
-    // blue_btn.rise(&btnRise);
+int main()                      // start
+{
+    flag = 0;                   // set flag = 0
+    led_A = 1;                  // set led_A on
+    led_B = 0;                  // set led_B off
+
+    blue_btn.fall(&btnFall);    // what happens when button is pressed
+    blue_btn.rise(&btnRise);    // what happens when button is released
+
+    while(true){}               // while(1)      
     
-    while(true){
-        if(flag == 1){
-            flag = 0;
-            printf("Button pressed \r\n");
-            // pressed = 1;
-            // released = 0;
-        } else if (flag == 2) {
-            flag = 0;
-            printf("Button released \r\n");
-            // pressed = 0;
-            // released = 1;
-        }
-    }
-
     return 0;
 }
